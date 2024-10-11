abeb1445a484df052d2393f982185f3304dfc1a2
Gabriel <96741685+gabrielkey@users.noreply.github.com>
2024-10-11 11:33:40 -0300

    Add files via upload

---
+++ Semana 04/Aula 1/rotas/produto.rota.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const validarProduto = require('../middleware/validarProduto.middleware');
+
+const produtos = {};
+
+router.post('/', validarProduto, (req, res) => {
+    const produto = req.body;
+    const idProduto = uuidv4();
+    produto.id = idProduto;
+    produtos[idProduto] = produto;
+    res.json({ msg: "Produto adicionado com sucesso!" });
+});
+
+router.get('/:id', (req, res) => {
+    res.json({ produto: produtos[req.params.id] });
+});
+
+router.put('/', validarProduto, (req, res) => {
+    const id = req.query.id;
+    if (id && produtos[id]) {
+        const produto = req.body;
+        produto.id = id;
+        produtos[id] = produto;
+        res.json({ msg: "Produto atualizado com sucesso!" });
+    } else {
+        res.status(400).json({ msg: "Produto não encontrado!" });
+    }
+});
+
+router.delete('/', (req, res) => {
+    const id = req.query.id;
+    if (id && produtos[id]) {
+        delete produtos[id];
+        res.json({ msg: "Produto deletado com sucesso!" });
+    } else {
+        res.status(400).json({ msg: "Produto não encontrado!" });
+    }
+});
+
+router.get('/', (req, res) => {
+    res.json({ produtos: Object.values(produtos) });
+});
+
+module.exports = router;
